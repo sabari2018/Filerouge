8c0828367a4826e7502d5f7aada3ca74ee9823aa
sabari2018 <37628129+sabari2018@users.noreply.github.com>
2020-06-19 14:07:56 +0200

    Update GamePlayer.java

--- src/players/GamePlayer.java
+++ src/players/GamePlayer.java
@@ -4,7 +4,7 @@ import games.AbstractGame;
 
 /**
  * Une interface qui factorise tout les types de joueurs Humain, Aleatoire, MinMax...
- * @author groupe cc 26
+ * @author SADOU BARRY
  */
 public interface GamePlayer {
 
@@ -12,21 +12,21 @@ public interface GamePlayer {
      * Une methode permettant a tout type de joueurs de choisir un coup
      * @param game Le jeu dans lequel le joueur choisi son coup
      * @return Le coup choisi
-     * @author groupe cc 26
+     * @author SADOU BARRY
      */
     public int chooseMove(AbstractGame game);
 
     /**
      * Une methode qui permet de retourner le nom du joueur
      * @return Le nom du joueur
-     * @author groupe cc 26
+     * @author SADOU BARRY
      */
     public String getName();
 
     /**
      * Une methode qui retourne l'icon du joueur
      * @return L'icon du joueur
-     * @author groupe cc 26
+     * @author SADOU BARRY
      */
     public String getIcon();
 }
